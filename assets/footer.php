13d2d6561decbdc0c98a820698eabdf19d35bc6e
NgayuPaul <paulmuju1@gmail.com>
2024-02-08 20:42:34 +0100

    Started working on the footer

---
+++ assets/footer.php
@@ -0,0 +1,25 @@
+
+    <section id="footer" class="footer">
+      <div class="download">
+        <h1>HORIZON PAY</h1>
+        <div class="footer-icon">
+          <i class="fa fa-facebook-square" style="color: blue"></i>
+          <i class="fa-brands fa-square-x-twitter"></i>
+          <i class="fa-brands fa-instagram"></i>
+          <i class="fa-brands fa-linkedin"></i>
+        </div>
+        <p>Get HorizonPay App on:</p>
+        <div class="apps">
+          <img
+            src="assets/images/apple-download.png"
+            alt=""
+            style="background-color: green;" />
+            <img
+            src="assets/images/android-download.png"
+            alt=""
+            style="background-color: green;" />
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <?php 
     $pagetitle = "Welcome to Horizon Pay";
-    require_once "assets/header.html";
+    require_once "assets/header.php";
 ?>
 
 <h1>Let's Create Together a payment System</h1>
+<?php 
+    require_once "assets/footer.php";
+?>
   
