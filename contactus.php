b43f19e8b83d39415bc1b52bb60ec72ebb2b260e
Alex Egbuna <alexegbuna77@gmail.com>
2024-02-08 08:18:05 -0800

    Alex files and pages

---
+++ contactus.php
@@ -0,0 +1,74 @@
+<?php 
+    require_once "assets/header.html";
+?>
+
+<?php
+ echo '<section>
+        <div id="mj1">
+          <div id="mj2">
+           <div id="mj3">Help & Support</div>
+           <div id="mj4">Contact Us</div>
+           <div id="mj5">Purple connect (24 hrs contact center)</div>
+          </div>
+          <div id="mj6">
+            <img src="assets/BBB.jpg" alt=""/>
+          </div>
+        </div>
+        <div id="">
+        <div id="">
+           <div id="">Contact Us</div>
+             <form action="">
+               <div id="">
+                 <div>First Name</div>
+                   <input type="text" placeholder="Your first name" title="please fill out this field." id="">
+               </div>
+               <div id="">
+                 <div>Last Name</div>
+                   <input type="text" placeholder="Your last name" title="please fill out this field." id="">
+               </div>
+               <div id="">
+                 <div>Email address</div>
+                   <input type="text" placeholder="example@email.com" title="please fill out this field." id="">
+               </div>
+               <div id="">
+                 <div>Phone Number</div>
+                   <input type="text" placeholder="08012345678" title="please fill out this field." id="">
+               </div>
+               <div id="">
+                 <div>Subject</div>
+                   <input type="text" placeholder="Complaint" title="please fill out this field." id="">
+               </div>
+               <div id="">
+                 <div>Department</div>
+                   <input type="text" placeholder="Customer Care" title="please fill out this field." id="">
+               </div>
+               <div>Message</div>
+                   <textarea placeholder="Type your message" name="" id="" cols="40" rows="8" title="please fill out this field."></textarea>
+               </div>
+               <div id="">Send</div>
+             </form>
+        </div>
+        <div>
+             <div id="">
+                <img src="" alt="" /><span id="">Help Desk</span>
+             </div>
+             <div id="">
+               <img src="assets/phone.png" alt="" /><span id="">+234-803-900-3700</span>
+             </div>
+             <div id="">
+                <img src="assets/phone.png" alt="" /><span id="">+234-01-277-7700-9</span>
+             </div>
+             <div id="">
+                <img src="assets/phone.png" alt="" /><span id="">07000PURPLE (07000787753)</span>
+             </div>
+             <div id="">
+                <img src="assets/Email.png" alt="" /><span id="">PURPLECONNECT@HORIZONPAY.COM</span>
+             </div>
+             <div id="">
+                <img src="" alt="" /><span id="">CHAT WITH US ON WHATSAPP - 09044411010</span>
+             </div>
+        </div>
+        </div>
+       </section>';
+?>
+
