dbcd83901daf1466334c517c4c19521d859140fd
Alex Egbuna <alexegbuna77@gmail.com>
2024-02-09 05:13:56 -0800

    Changes made and comment out for easy understanding

--- contactus.php
+++ contactus.php
@@ -1,73 +1,50 @@
-<?php 
-    require_once "assets/header.html";
+<?php
+     require_once "index.php";
 ?>
 
 <?php
 echo '<section>
-<div id="mj1">
-  <div id="mj2">
-   <div id="mj3">Help & Support</div>
-   <div id="mj4">Contact Us</div>
-   <div id="mj5">Purple connect (24 hrs contact center)</div>
+<!-- First div Section holding both the Text and the Image id="1" you can remove it onces seen, id="2" holds the text-->
+<div id="1">
+  <div id="2">
+   <div>Help & Support</div>
+   <div>Contact Us</div>
+   <div>Purple connect (24 hrs contact center)</div>
   </div>
-  <div id="mj6">
+  <!-- id="3" holds the images -->
+  <div id="3">
     <img src="assets/Images/BBB.jpg" alt=""/>
   </div>
 </div>
-<div id="">
-<div id="">
-   <div id="">Contact Us</div>
+<!-- Second div Section id="4" holds the Form -->
+<div id="4">
      <form action="">
-       <div id="">
+         <div>Contact Us</div>
          <div>First Name</div>
            <input type="text" placeholder="Your first name" title="please fill out this field." id="">
-       </div>
-       <div id="">
          <div>Last Name</div>
            <input type="text" placeholder="Your last name" title="please fill out this field." id="">
-       </div>
-       <div id="">
          <div>Email address</div>
            <input type="text" placeholder="example@email.com" title="please fill out this field." id="">
-       </div>
-       <div id="">
          <div>Phone Number</div>
            <input type="text" placeholder="08012345678" title="please fill out this field." id="">
-       </div>
-       <div id="">
          <div>Subject</div>
            <input type="text" placeholder="Complaint" title="please fill out this field." id="">
-       </div>
-       <div id="">
          <div>Department</div>
            <input type="text" placeholder="Customer Care" title="please fill out this field." id="">
-       </div>
-       <div>Message</div>
+         <div>Message</div>
            <textarea placeholder="Type your message" name="" id="" cols="40" rows="8" title="please fill out this field."></textarea>
-       </div>
-       <div id="">Send</div>
+         <div id="">Send</div>
      </form>
 </div>
-<div>
-     <div id="">
-        <img src="" alt="" /><span id="">Help Desk</span>
-     </div>
-     <div id="">
-       <img src="assets/Images/phone.png" alt="" /><span id="">+234-803-900-3700</span>
-     </div>
-     <div id="">
+<!-- id="5" holds all the image icon and the span for text beside it incase of styling -->
+<div id="5"> 
+        <img src="assets/Images/Email.png" alt="" /><span id="">Help Desk</span>
+        <img src="assets/Images/phone.png" alt="" /><span id="">+234-803-900-3700</span>
         <img src="assets/Images/phone.png" alt="" /><span id="">+234-01-277-7700-9</span>
-     </div>
-     <div id="">
         <img src="assets/Images/phone.png" alt="" /><span id="">07000PURPLE (07000787753)</span>
-     </div>
-     <div id="">
         <img src="assets/Images/Email.png" alt="" /><span id="">PURPLECONNECT@HORIZONPAY.COM</span>
-     </div>
-     <div id="">
-        <img src="" alt="" /><span id="">CHAT WITH US ON WHATSAPP - 09044411010</span>
-     </div>
-</div>
+        <img src="assets/Images/Email.png" alt="" /><span id="">CHAT WITH US ON WHATSAPP - 09044411010</span>
 </div>
 </section>';
 ?>
--- index.php
+++ index.php
@@ -8,3 +8,4 @@
     require_once "assets/footer.php";
 ?>
   
+  
