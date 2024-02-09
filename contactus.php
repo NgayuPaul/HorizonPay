f0552a91f6f19889775d1c0b398396be4b22719e
Alex Egbuna <alexegbuna77@gmail.com>
2024-02-09 15:19:29 -0800

    Third Time

--- contactus.php
+++ contactus.php
@@ -5,51 +5,50 @@
      require_once "assets/header.php";
 ?>
 
-<?php
-echo '<section>
-<!-- First div Section holding the Text id="1", you can remove it onces seen, id="2" holds the Image-->
-  <div id="1">
-   <div>Help & Support</div>
-   <div>Contact Us</div>
-   <div>Purple connect (24 hrs contact center)</div>
+
+<section>
+<!-- First div Section1 holding the Text class="text1" and img class="img2" you can rename it onces seen-->
+<div id="Section1">
+  <div class="text1">
+   <p class="Help">Help & Support</p>
+   <h1 class="Contact">Contact Us</h1>
+   <p class="Purple">Purple connect (24 hrs contact center)</p>
   </div>
-  <!-- id="2" holds the images -->
-  <div id="2">
+  <!-- class="img2" holds the images -->
+  <div class="img2">
     <img src="assets/Images/BBB.jpg" alt=""/>
   </div>
-<!-- Second div Section id="3" holds the Form -->
-<div id="3">
+  </div>
+<!-- Second div Section2 holds the class="3Form" and class="4img" img -->
+<div id="Section2">
+<div class="3form">
      <form action="">
-         <div>Contact Us</div>
-         <div>First Name</div>
+         <h2 class="CU">Contact Us</h2>
+         <div class="FN">First Name</div>
            <input type="text" placeholder="Your first name" title="please fill out this field." id="">
-         <div>Last Name</div>
+         <div class="LN">Last Name</div>
            <input type="text" placeholder="Your last name" title="please fill out this field." id="">
-         <div>Email address</div>
+         <div class="EA">Email address</div>
            <input type="text" placeholder="example@email.com" title="please fill out this field." id="">
-         <div>Phone Number</div>
+         <div class="PN">Phone Number</div>
            <input type="text" placeholder="08012345678" title="please fill out this field." id="">
-         <div>Subject</div>
+         <div class="SB">Subject</div>
            <input type="text" placeholder="Complaint" title="please fill out this field." id="">
-         <div>Department</div>
+         <div class="DP">Department</div>
            <input type="text" placeholder="Customer Care" title="please fill out this field." id="">
-         <div>Message</div>
+         <div class="MSG">Message</div>
            <textarea placeholder="Type your message" name="" id="" cols="40" rows="8" title="please fill out this field."></textarea>
-         <div id="">Send</div>
+         <div class="SEND">Send</div>
      </form>
 </div>
-<!-- id="4" holds all the image icon and the span for text beside it incase of styling -->
-<div id="4"> 
-        <img src="assets/Images/Email.png" alt="" /><span id="">Help Desk</span>
-        <img src="assets/Images/phone.png" alt="" /><span id="">+234-803-900-3700</span>
-        <img src="assets/Images/phone.png" alt="" /><span id="">+234-01-277-7700-9</span>
-        <img src="assets/Images/phone.png" alt="" /><span id="">07000PURPLE (07000787753)</span>
-        <img src="assets/Images/Email.png" alt="" /><span id="">PURPLECONNECT@HORIZONPAY.COM</span>
-        <img src="assets/Images/Email.png" alt="" /><span id="">CHAT WITH US ON WHATSAPP - 09044411010</span>
+<!-- class="4img" holds all the image icon and the span for text beside it incase of styling -->
+<div class="4img"> 
+        <img src="assets/Images/Email.png" alt="" /><span class="hlp">Help Desk</span>
+        <img src="assets/Images/phone.png" alt="" /><span class="num">+234-803-900-3700</span>
+        <img src="assets/Images/phone.png" alt="" /><span class="nom">+234-01-277-7700-9</span>
+        <img src="assets/Images/phone.png" alt="" /><span class="nuom">07000PURPLE (07000787753)</span>
+        <img src="assets/Images/Email.png" alt="" /><span class="plc">PURPLECONNECT@HORIZONPAY.COM</span>
+        <img src="assets/Images/Email.png" alt="" /><span class="cwu">CHAT WITH US ON WHATSAPP - 09044411010</span>
 </div>
-</section>';
-?>
-
-<?php
-     require_once "assets/footer.php";
-?>
+</div>
+</section>
